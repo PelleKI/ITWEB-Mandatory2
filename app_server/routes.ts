705b288f6aa666119b607fdaccf036ff06e25bf1
pelle <pelle@flork.dk>
2017-09-22 14:03:08 +0200

    Enabled CORS on WorkoutProgramController

--- app_server/routes.ts
+++ app_server/routes.ts
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
 import { WorkoutControllerRoutes } from './controllers/WorkoutProgramController';
+import * as cors from 'cors';
 
 /* Example web api controller*/
-router.use('/api/workoutprogram', WorkoutControllerRoutes);
+router.use('/api/workoutprogram', cors(), WorkoutControllerRoutes);
 
 export = router;
